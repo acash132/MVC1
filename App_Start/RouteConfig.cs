dbea33aeb4068311b0c7565d53da31766b2170b7
akash <akash@DESKTOP-B1PDELG>
2026-05-01 11:54:53 +0530

    View Data Implemented in MVC

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVCFirst
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Student", action = "StudentDetails", id = UrlParameter.Optional }
+                defaults: new { controller = "Product", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFirst.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            int ProductId = 1;
+            string ProductName = "Apple";
+            string Quantity = "1 Dozen";
+            decimal Price = 100.00m;
+            bool Status = false;
+            ViewData["ProductId"] = ProductId;
+            ViewData["ProductName"] = ProductName;
+            ViewData["Quantity"] = Quantity;
+            ViewData["Price"] = Price;
+            ViewData["Status"] = Status;
+
+            return View();
+        }
+    }
+}
