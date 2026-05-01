e435acebfc317694c9f60432f0c83a85f0fcbd0a
akash <akash@DESKTOP-B1PDELG>
2026-05-01 15:24:46 +0530

    Viewbag,ViewData,Temp data, Session data

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVCFirst
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Product", action = "Index1", id = UrlParameter.Optional }
             );
         }
     }
--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -24,5 +24,35 @@ namespace MVCFirst.Controllers
 
             return View();
         }
+        public ActionResult Index1()
+        {
+            int ProductId = 1;
+            string ProductName = "Apple";
+            string Quantity = "1 Dozen";
+            decimal Price = 100.00m;
+            bool Status = false;
+           ViewBag.ProductId = ProductId;
+            ViewBag.ProductName = ProductName;
+            ViewBag.Quantity = Quantity;
+            ViewBag.Price = Price;
+            ViewBag.Status = Status;
+
+            return View();
+        }
+        public ActionResult Redirect() {
+            ViewBag.messages = "This is a view bag message";
+            ViewData["message"] = "This is a view data message";
+            TempData["Data"] = "This is a temp data message";
+            Session["SessionData"] = "This is a session data message";
+
+            return RedirectToAction("Success");
+        
+        }
+        public ActionResult Success() {
+           
+            return View();
+        }
+
+
     }
 }
