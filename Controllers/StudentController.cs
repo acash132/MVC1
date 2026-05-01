7c473aacbdcaa733a4b25cf753a86acfb0a0b879
akash <akash@DESKTOP-B1PDELG>
2026-05-01 11:16:48 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using MVCFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFirst.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult IndexStud()
+        {
+            return View();
+        }
+        public ActionResult StudentDetails()
+        {
+            StudentModel student = new StudentModel();
+            {
+                student.Id = 1;
+                student.Name = "Akash";
+                student.Address = "Vanasthalipuram";
+            }
+            return View(student);
+        }
+    }
+}
